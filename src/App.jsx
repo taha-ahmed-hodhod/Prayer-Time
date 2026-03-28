269683280f4332b9fd23800fc5ef1059daa98951
taha ahmed <tahahodhod650@gmail.com>
2026-03-29 01:06:39 +0200

    the project is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+import Prayer from "./components/Prayer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [timePrayer, setTimePrayer] = useState({})
+  const [dateHijri, setDateHijiri] = useState("")
+  const [cityName, setCityName] = useState("Cairo")
 
+  const governorates = [
+    { name: "القاهرة", value: "Cairo" },
+    { name: "الجيزة", value: "Giza" },
+    { name: "الإسكندرية", value: "Alexandria" },
+    { name: "الدقهلية", value: "Dakahlia" },
+    { name: "البحر الأحمر", value: "Red_sea" },
+    { name: "البحيرة", value: "Beheira" },
+    { name: "الفيوم", value: "Fayoum" },
+    { name: "الغربية", value: "Gharbia" },
+    { name: "الإسماعيلية", value: "Ismailia" },
+    { name: "المنوفية", value: "Menofia" },
+    { name: "المنيا", value: "Minya" },
+    { name: "القليوبية", value: "Qalyubia" },
+    { name: "الوادي الجديد", value: "New_valley" },
+    { name: "السويس", value: "Suez" },
+    { name: "أسوان", value: "Aswan" },
+    { name: "أسيوط", value: "Assiut" },
+    { name: "بني سويف", value: "Beni_suef" },
+    { name: "بورسعيد", value: "Port_said" },
+    { name: "دمياط", value: "Damietta" },
+    { name: "جنوب سيناء", value: "South_sinai" },
+    { name: "شمال سيناء", value: "North_sinai" },
+    { name: "سوهاج", value: "Sohag" },
+    { name: "قنا", value: "Qena" },
+    { name: "كفر الشيخ", value: "Kafr_el_sheikh" },
+    { name: "مطروح", value: "Matrouh" },
+    { name: "الأقصر", value: "Luxor" },
+    { name: "الشرقية", value: "Sharqia" },
+  ];
+
+
+  const date = new Date();
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = String(date.getFullYear());
+  const dateToday = `${day}-${month}-${year}`;
+
+  useEffect(() => {
+    const prayerTime = async () => {
+      try {
+        const response = await fetch(
+          `https://api.aladhan.com/v1/timingsByCity/${dateToday}?city=${cityName}&country=Egypt`
+        );
+        const time = await response.json();
+              console.log("Full Response:", time); // ← حطها
+      console.log("City:", cityName);  
+
+        const time_obj = time.data.timings
+        setTimePrayer(time_obj)
+        setDateHijiri(time.data.date.hijri.date)
+      } catch (err) {
+        console.log("Error for giving Data", err);
+      }
+    };
+    prayerTime();
+  }, [cityName]);
+
+  const formateTimes = (time) => {
+    if(!time) {
+      return"00:00"
+    }
+
+    let [hours, minutes] = time.split(":").map(Number);
+    const perd = hours >= 12 ? "PM" : "AM" ;
+    hours = hours % 12 || 12;
+    return `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : `${minutes}`}  ${perd}`
+
+  }
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <section>
+      <div className="container">
+        <div className="top_sec">
+          <div className="city">
+            <h3>المدينة</h3>
+            <select name="" id="" onChange={(e) => setCityName(e.target.value)}>
+              {governorates.map((city, index) => {
+                return (
+                  <option value={city.value} key={index}>
+                    {city.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="date">
+            <h3>التاريخ</h3>
+            <h4>{dateToday}  ميلادي</h4>
+            <h4>{dateHijri}  هجري</h4>
+          </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+        <Prayer name={"الفجر"} time={formateTimes(timePrayer.Fajr)} />
+        <Prayer name={"الشروق"} time={formateTimes(timePrayer.Sunrise)} />
+        <Prayer name={"الظهر"} time={formateTimes(timePrayer.Dhuhr)} />
+        <Prayer name={"العصر"} time={formateTimes(timePrayer.Asr)} />
+        <Prayer name={"المغرب"} time={formateTimes(timePrayer.Maghrib)} />
+        <Prayer name={"العشاء"} time={formateTimes(timePrayer.Isha)} />
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Prayer.jsx
@@ -0,0 +1,11 @@
+
+const Prayer = ({name, time}) => {
+  return (
+    <div className="prayer">
+        <p className="name_prayer">{name}</p>
+        <p className="time_prayer">{time}</p>
+    </div>
+  )
+}
+
+export default Prayer
